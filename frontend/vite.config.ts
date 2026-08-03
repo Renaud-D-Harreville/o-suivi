f2a565a801b614175fc665b926e26ab70ba1aaee
renauddharreville <renaud-d-harreville-contractor@naverlabs.com>
2026-08-03 08:15:20 +0200

    Ajout d'un logo png

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,15 +18,25 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "logo.svg",
-            sizes: "any",
-            type: "image/svg+xml",
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
           {
             src: "logo.svg",
             sizes: "any",
             type: "image/svg+xml",
-            purpose: "maskable",
           },
         ],
       },
