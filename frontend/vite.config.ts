b94152a901ebb08d71de34a4ddfc579ea95b6e77
renauddharreville <renaud-d-harreville-contractor@naverlabs.com>
2026-08-03 10:25:17 +0200

    enhancing icon

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: "prompt",
-      includeAssets: ["favicon.svg", "logo.svg"],
+      includeAssets: ["favicon.svg", "logo.svg", "pwa-192x192.png", "pwa-512x512.png"],
       manifest: {
         name: "O-Suivi",
         short_name: "O-Suivi",
